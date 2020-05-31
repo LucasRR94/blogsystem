896e1347e8e30ac1dd5550d78a11f428f32c3bf3
Lucas Rocha <lucasregodarocha@outlook.com>
2020-05-30 21:24:58 -0300

    create page to list all users of API

---
+++ src/components/BtnListUsers.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="main">
+    <div class="main-wrapper">
+      <button>List Users</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'BtnListUsers',
+}
+</script>
+
+<style lang='scss'>
+  @import '../style/modulos.scss';
+  html,*,body{
+    margin: 0 0;
+    padding: 0 0;
+    box-sizing:border-box;
+  }
+  .main{
+    width:100vw;
+    height:40%;
+    display:flex;
+    flex-direction:column;
+    flex:1;
+    .main-wrapper{
+      @include positioning-element-flex(center,center);
+      width:90vw;
+      height: 20%;
+      display:flex;
+      flex-direction:column;
+      justify-content:center;
+      button{
+        @include positioning-element-flex(center,center);
+        font-size:1rem;
+        width:80px;
+        height:60px;
+        background:black;
+        color:rgb(119, 119, 119);
+        border:2px solid rgb(119, 119, 119);
+        border-radius:.4rem;
+        outline:none;
+      }
+      //@include effect-portable-all-browsers(active,button,'scalling');
+    }
+  }
+</style>
---
+++ src/views/ListUsers.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="main">
+    <header>
+      <div class="area-logo">
+        <LogoComponent/>
+      </div>
+    </header>
+    <main> 
+      <div class="area-search">
+        <BtnListUsers/>
+      </div>
+      <div class="results-search">
+        <LoadAndSearchUsers v-bind:querySearch="searchQuery"/>
+      </div>
+    </main>
+    <footer>
+      <FooterComponent/>
+    </footer>
+  </div>
+</template>
+
+<script>
+import LogoComponent from '../components/LogoComponent.vue'
+import FooterComponent from '../components/FooterComponent.vue'
+import SearchUserArea from '../components/SearchUserArea.vue'
+import BtnListUsers from '../components/BtnListUsers.vue'
+import LoadAndSearchUsers from '../components/LoadAndSearchUsers.vue'
+export default {
+  name: 'ListUsers',
+  data(){
+    return{
+      searchQuery:''
+    }
+  },
+  components: {LoadAndSearchUsers,BtnListUsers,SearchUserArea,LogoComponent,FooterComponent},
+  methods:{
+    updateQuery(newQuery){
+      this.searchQuery = newQuery;
+    }
+  }
+}
+</script>
+
+<style>
+  .main{
+    width:100vw;
+    height:100vh;
+  }
+</style>
