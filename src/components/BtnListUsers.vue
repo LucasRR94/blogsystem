d1c651a0561031f7b3818d57821a033e4540b2aa
Lucas Rocha <lucasregodarocha@outlook.com>
2020-05-31 18:07:32 -0300

    developed the final features of listing users

--- src/components/BtnListUsers.vue
+++ src/components/BtnListUsers.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="main">
     <div class="main-wrapper">
-      <button>List Users</button>
+      <button v-on:click="$emit('requestedList')">List Users</button>
     </div>
   </div>
 </template>
@@ -21,29 +21,37 @@ export default {
   }
   .main{
     width:100vw;
-    height:40%;
+    height:auto;
     display:flex;
     flex-direction:column;
     flex:1;
+    max-height: 200px;
     .main-wrapper{
+      max-height: 200px;
       @include positioning-element-flex(center,center);
       width:90vw;
-      height: 20%;
+      height: auto;
       display:flex;
       flex-direction:column;
       justify-content:center;
       button{
         @include positioning-element-flex(center,center);
         font-size:1rem;
-        width:80px;
+        width:90px;
         height:60px;
         background:black;
         color:rgb(119, 119, 119);
         border:2px solid rgb(119, 119, 119);
         border-radius:.4rem;
         outline:none;
+        &:active:hover{
+          border:2px solid black;
+        }
+        &:hover{
+          background:rgb(11, 11, 49);
+          color:white;
+        }
       }
-      //@include effect-portable-all-browsers(active,button,'scalling');
     }
   }
 </style>
--- src/views/ListUsers.vue
+++ src/views/ListUsers.vue
@@ -6,11 +6,11 @@
       </div>
     </header>
     <main> 
-      <div class="area-search">
-        <BtnListUsers/>
+      <div class="list-btn">
+        <BtnListUsers v-on:requestedList="genList"/>
       </div>
-      <div class="results-search">
-        <LoadAndSearchUsers v-bind:querySearch="searchQuery"/>
+      <div class="list-search">
+        <MountListUsers v-bind:searchRequested="searchRequested"/>
       </div>
     </main>
     <footer>
@@ -22,28 +22,73 @@
 <script>
 import LogoComponent from '../components/LogoComponent.vue'
 import FooterComponent from '../components/FooterComponent.vue'
-import SearchUserArea from '../components/SearchUserArea.vue'
 import BtnListUsers from '../components/BtnListUsers.vue'
-import LoadAndSearchUsers from '../components/LoadAndSearchUsers.vue'
+import MountListUsers from '../components/MountListUsers.vue'
+
 export default {
   name: 'ListUsers',
+  props:{},
   data(){
     return{
-      searchQuery:''
+      searchQuery:'',
+      searchRequested:false
     }
   },
-  components: {LoadAndSearchUsers,BtnListUsers,SearchUserArea,LogoComponent,FooterComponent},
+  components: {MountListUsers,BtnListUsers,LogoComponent,FooterComponent},
   methods:{
     updateQuery(newQuery){
       this.searchQuery = newQuery;
+    },
+    genList(){
+      this.searchRequested = true;
     }
   }
 }
 </script>
 
-<style>
+<style lang='scss'>
+@import '../style/modulos.scss';
+  html,*,body{
+    padding: 0 0;
+    margin: 0 0;
+    box-sizing:border-box;
+  }
   .main{
+    display:grid;
+    grid-template-rows:2fr .5fr .5fr;
+    //grid-template-rows:auto;
+    justify-content: center;
+    align-content: space-between;
     width:100vw;
     height:100vh;
+    header {
+      width:100vw;
+      height:auto;
+      .area-logo{
+        margin:0.5rem;
+      }
+    }
+    main{
+      width:100vw;
+      height:auto;
+      display:grid;
+      grid-template-rows:.1fr 1.9fr;
+      justify-content: center;
+      align-content: center;
+      .list-btn{
+        width:100vw;
+        height:auto;
+      } 
+      .list-search{
+        width:100vw;
+        height:auto;
+      }
+    }
+    footer{
+      margin-top:.5rem;
+      align-self:end;
+      width:100vw;
+      height:auto;
+    }
   }
 </style>
