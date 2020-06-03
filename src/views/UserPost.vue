81aef892a977efb71951198e0de73f107ddf5530
Lucas Rocha <lucasregodarocha@outlook.com>
2020-06-02 22:07:53 -0300

    file deleted src/components/ProfileArea

---
+++ src/views/UserPost.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="main-user-post"></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  html,*,body{
+    margin: 0 0;
+    padding: 0 0;
+    box-sizing:border-box;
+  }
+  .main-user-post{
+    width:80vw;
+  }
+</style>
