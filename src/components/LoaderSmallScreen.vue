7595ae354ba2e708592b7004bc3fcc13142a5cd9
Lucas Rocha <lucasregodarocha@outlook.com>
2020-06-15 14:23:49 -0300

    adding message of failure request

---
+++ src/components/LoaderSmallScreen.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="loader-small-screen">
+    <div class="loader-small-screen-wrapper">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'LoaderSmallScreen'
+}
+</script>
+
+<style lang='scss'>
+  @import "../style/_modulos.scss";
+  .loader-small-screen{
+    width:400px;
+    height:400px;
+    @include center-div;
+    &-wrapper{
+      width:100px;
+      height:100px;
+      border-radius:50%;
+      // background:black;
+      animation:changeBorderColor 2s linear infinite;
+    }
+    @keyframes changeBorderColor{
+      0%{
+        border-top:2px solid black;
+        border-left:2px solid white;
+        border-right:2px solid white;
+        border-bottom:2px solid white;
+      }
+      25%{
+        border-top:2px solid white;
+        border-left:2px solid white;
+        border-right:2px solid black;
+        border-bottom:2px solid white;
+      }
+      50%{
+        border-top:2px solid white;
+        border-left:2px solid white;
+        border-right:2px solid white;
+        border-bottom:2px solid black;
+      }
+      75%{
+        border-top:2px solid white;
+        border-left:2px solid black;
+        border-right:2px solid white;
+        border-bottom:2px solid white;
+      }
+      100%{
+        border-top:2px solid black;
+        border-left:2px solid black;
+        border-right:2px solid black;
+        border-bottom:2px solid black;
+      }
+    }
+  }
+</style>
--- src/components/MountListUsers.vue
+++ src/components/MountListUsers.vue
@@ -1,27 +1,39 @@
 <template>
   <div class="main-load-and-search-users">
-    <ul class="main-load-and-search-users-area-profile" v-if="searchRequested">
-      <li class="main-load-and-search-users-area-profile-profile-card" v-for="user in contentConsultUsersLinks" :key="user.id"> 
-        <router-link :to="{ name: 'User', params: { idUser: user.id }}"> 
-          <div class="container-profile">
-            <div class="container-profile-pic">
-              <img v-bind:src="require(`../assets/${picturePath}`)" alt="picture-user">
+    <div class="main-load-and-search-users-wrapper" v-if="searchRequested">
+      <ul class="main-load-and-search-users-wrapper-area-profile" v-if="dataAvailable">
+        <li class="main-load-and-search-users-wrapper-area-profile-profile-card" v-for="user in contentConsultUsersLinks" :key="user.id"> 
+          <router-link :to="{ name: 'User', params: { idUser: user.id }}"> 
+            <div class="container-profile">
+              <div class="container-profile-pic">
+                <img v-bind:src="require(`../assets/${picturePath}`)" alt="picture-user">
+              </div>
+              <div class="container-profile-name">
+                <p>{{user.name}}</p>
+              </div>
             </div>
-            <div class="container-profile-name">
-              <p>{{user.name}}</p>
-            </div>
-          </div>
-        </router-link>  
-      </li>
-    </ul>
+          </router-link>  
+        </li>
+      </ul>
+      <div class="main-load-and-search-users-wrapper-error-area" v-else>
+        <article class="main-load-and-search-users-wrapper-error-area-msg" v-if="errorLoadingAPIContent">
+          <p>{{ErrorMessage}}</p>
+        </article>
+        <div class="main-load-and-search-users-wrapper-error-area-loadscreen" v-else>
+          <LoaderSmallScreen/>           
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import LoaderSmallScreen from '../components/LoaderSmallScreen.vue';
 
 export default {
   name:'MountListUsers',
+  components:{LoaderSmallScreen,axios},
   props:{
     searchRequested:{
       type:Boolean,
@@ -32,20 +44,29 @@ export default {
     return{
       contentConsultUsersLinks:[],
       picturePath:"pictureUnknown.svg",
+      ErrorMessage:"Error detected,unfortly don't was possible load response from API, try later.",
+      dataAvailable:false,
+      errorLoadingAPIContent:false
     }
   },
   methods:{
     getUsers(){
       axios({method: 'get',
         url: 'https://jsonplaceholder.typicode.com/users',
-        headers: {
-          "Access-Control-Allow-Origin": "*",
+        headers: {  
+          "Access-Control-Allow-Origin": "localhost",
           "Content-type": "application/json; charset=UTF-8"
         }
       })
-      .then(response =>
-      this.contentConsultUsersLinks= response.data)
-      .catch(err => console.log(`Error detected:${err}`));
+      .then(response =>{
+        this.dataAvailable = true;
+        this.contentConsultUsersLinks = response.data;
+      })
+      .catch(err => {
+        console.log(`Error detected:${err.response}`);
+        this.dataAvailable = false;
+        this.errorLoadingAPIContent = true; 
+      });
     }
   },
   mounted(){
@@ -55,7 +76,7 @@ export default {
 </script>
 
 <style lang="scss">
-  @import "../style/_modulos.scss";
+  @import "../style/_configs.scss","../style/_modulos.scss";
   
   *,html,body{
     margin: 0 0;
@@ -66,49 +87,63 @@ export default {
     width:100vw;
     height:auto;
     @include center-div;
-    &-area-profile{
-      width:98vw;
-      max-width:600px;
+    &-wrapper{
+      width:100vw;
       height:auto;
-      &-profile-card{
-        list-style: none;
-        a{
-          text-decoration:none;
-          font-size:1.2rem;
-          .container-profile{
-            width:auto;
-            height:auto;
-            margin:1rem .5rem;
-            border-radius:0.4rem;
-            background:#5f5f5f;
-            color:black;
-            display:grid;
-            @include positioning-two-elements-flex-midle(row);
-            &-pic{
-              @include positioning-element-flex(center,center);
-              background:black;
-              width:100px;
-              height:100px;
-              border-radius:.4rem 0 0 .4rem;
-              img{
-                margin:.5rem;
-                width:80px;
-                height:80px;
+      @include center-div;
+      &-area-profile{
+        width:98vw;
+        max-width:600px;
+        height:auto;
+        &-profile-card{
+          list-style: none;
+          a{
+            text-decoration:none;
+            font-size:1.2rem;
+            .container-profile{
+              width:auto;
+              height:auto;
+              margin:1rem .5rem;
+              border-radius:0.4rem;
+              background:#5f5f5f;
+              color:black;
+              display:grid;
+              @include positioning-two-elements-flex-midle(row);
+              &-pic{
+                @include positioning-element-flex(center,center);
+                background:black;
+                width:100px;
+                height:100px;
+                border-radius:.4rem 0 0 .4rem;
+                img{
+                  margin:.5rem;
+                  width:80px;
+                  height:80px;
+                }
               }
-            }
-            &-name{
-              @include positioning-element-flex(center,center);
-              text-decoration:none;
-              font-weight: lighter;
-              margin:.5rem;
-            }
-            &-name:hover{
-              color:white;
+              &-name{
+                @include positioning-element-flex(center,center);
+                text-decoration:none;
+                font-weight: lighter;
+                margin:.5rem;
+              }
+              &-name:hover{
+                color:white;
+              }
             }
           }
         }
       }
+      &-error-area{
+        width:80vw;
+        height:auto;
+        font-weight: lighter;
+        color:$grey-color-font;
+        @include center-div;
+        p{
+          max-width:600px;
+        }
+      }
     }
-
   }
 </style>
